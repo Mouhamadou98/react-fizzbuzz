87df72f89310c32b4053f50fb3591f58320139aa
Mouhamadou98 <modzigning@gmail.com>
2022-02-18 10:40:17 +0000

    ReactFizzBuzz complete mais le test est toujours sur le composant App

--- src/FizzBuzz.test.js
+++ src/FizzBuzz.test.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import { render, fireEvent,screen } from "@testing-library/react";
 import "@testing-library/jest-dom"
-// import "jest-dom/extend-expect";
+//import "jest-dom/extend-expect";
 // import FizzBuzz from "./FizzBuzz";
 import App from "./App";
+import './FizzBuzz'
 
 it('renders counter start at 1', () => {
     render(<App />);
+
     expect(screen.getByTestId("fizz-buzz")).toHaveTextContent("1");
 })
 
@@ -18,5 +20,33 @@ it('Should increment the counter to 1 when we click play', () => {
 
     fireEvent.click(screen.getByText("Play"));
     expect(screen.getByTestId("fizz-buzz")).toHaveTextContent("fizz")
+
+    fireEvent.click(screen.getByText("Play"));
+    expect(screen.getByTestId("fizz-buzz")).toHaveTextContent("4")
+
+    fireEvent.click(screen.getByText("Play"));
+    expect(screen.getByTestId("fizz-buzz")).toHaveTextContent("buzz")
+
+    fireEvent.click(screen.getByText("Play"));
+    expect(screen.getByTestId("fizz-buzz")).toHaveTextContent("fizz")
+
+    fireEvent.click(screen.getByText("Play"));
+    expect(screen.getByTestId("fizz-buzz")).toHaveTextContent("7")
+
+    fireEvent.click(screen.getByText("Play"));
+    fireEvent.click(screen.getByText("Play"));
+    fireEvent.click(screen.getByText("Play"));
+    fireEvent.click(screen.getByText("Play"));
+    fireEvent.click(screen.getByText("Play"));
+    fireEvent.click(screen.getByText("Play"));
+
+    expect(screen.getByTestId("fizz-buzz")).toHaveTextContent("fizz")
+    for (let index = 13; index < 53; index++) {
+        fireEvent.click(screen.getByText("Play"));
+        if(index > 51){
+            // eslint-disable-next-line jest/no-conditional-expect
+            expect(screen.getByTestId("fizz-buzz")).toHaveTextContent("buzz")
+        }
+    }
 
 })
